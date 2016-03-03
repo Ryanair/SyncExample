3c2b61eafe86668626c5202666b904603f50e167
atanasovv <atanasovv@ryanair.com>
2016-03-03 17:01:22 +0000

    added database singleton

--- app/src/main/java/com/ryanair/syncexample/AppController.java
+++ app/src/main/java/com/ryanair/syncexample/AppController.java
@@ -1,7 +1,55 @@
 package com.ryanair.syncexample;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.android.AndroidContext;
+
+import java.io.IOException;
 
 public class AppController extends Application {
+    private static final String TAG = "AppController";
+
+    private static Database database;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        openDatabase();
+    }
+
+    private void openDatabase() {
+        Manager manager;
+
+        if (!Manager.isValidDatabaseName(Constants.DATABASE_NAME)) {
+            Log.e(TAG, "Bad database name");
+            return;
+        }
+
+        try {
+            manager = new Manager(new AndroidContext(getBaseContext()), Manager.DEFAULT_OPTIONS);
+        } catch (IOException e) {
+            Log.e(TAG, "Cannot create manager object");
+            return;
+        }
+
+        try {
+            database = manager.getDatabase(Constants.DATABASE_NAME);
+        } catch (CouchbaseLiteException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    public Database getDatabase() {
+        if(database == null) {
+            openDatabase();
+        }
+
+        return database;
+    }
 
 }
---
+++ app/src/main/java/com/ryanair/syncexample/Constants.java
@@ -0,0 +1,9 @@
+package com.ryanair.syncexample;
+
+
+public class Constants {
+    public static final String DATABASE_NAME = "reference_data";
+    public static final String syncUrl = "http://192.168.56.1:4984/reference_data";
+    public static final String[] channels = new String[]{"ref_data_v1"};
+
+}
